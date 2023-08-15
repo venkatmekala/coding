5b4305f9a3fe3d13e8fd8b16bd1d2369d16923c5
venkatmekala <36647280+venkatmekala@users.noreply.github.com>
2023-08-14 21:50:33 -0400

    Changes

--- threads/src/com/javabites/synchronization/ThreadSynchronization.java
+++ threads/src/com/javabites/synchronization/ThreadSynchronization.java
@@ -5,9 +5,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadSynchronization {
     static AtomicInteger count = new AtomicInteger();
+    //static int count = 0;
     private synchronized static void increment() {
         //compound operation
+        //count++;
         count.getAndIncrement();
+
     }
     public static void main(String[] args) {
 
@@ -18,7 +21,7 @@ public class ThreadSynchronization {
 
         Thread t2 = new Thread(() -> {
             for (int i = 1; i <= 10000; i++)
-                count.getAndIncrement();
+               increment();
         });
 
         t1.start();
