30efd4ec62d2651763fb4069b9ba7db9480baedb
Venkat <venkatsku.java@gmail.com>
2023-08-30 20:02:22 -0400

    First Commit

--- src/main/java/com/javabites/arrays/easy/ArraySpiral.java
+++ src/main/java/com/javabites/arrays/easy/ArraySpiral.java
@@ -3,7 +3,8 @@ package com.javabites.arrays.easy;
 public class ArraySpiral {
 
     public static void main(String[] args) {
-        int[] array = {1, 10, 14, 20, 18, 12};
+        //int[] array = {1, 10, 14, 20, 18, 12};
+        int[] array = { 1, 10, 14, 20, 18, 12, 5 };
         int end = array.length - 1;
         int start = 0;
         while(start < end) {
@@ -14,7 +15,7 @@ public class ArraySpiral {
             }
             start++;
 
-            if(array[end] < array[start]) {
+            if(array[end] > array[start]) {
                 System.out.println("No");
                 return;
             }
